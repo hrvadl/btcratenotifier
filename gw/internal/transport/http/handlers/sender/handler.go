eff0de86e3c1091cc2e3a88930855256c1b72afe
Vadym Hrashchenko <vhrashch@cisco.com>
2024-05-17 14:45:58 +0300

    feat: remove send email public endpoint

--- gw/internal/transport/http/handlers/sender/handler.go
+++ gw/internal/transport/http/handlers/sender/handler.go
@@ -9,7 +9,6 @@ import (
 
 type Service interface {
 	Subscribe(ctx context.Context, mail string) error
-	SendToAll(ctx context.Context) error
 }
 
 func NewHandler(svc Service, log *slog.Logger) *Handler {
@@ -38,19 +37,4 @@ func (h *Handler) Subscribe(w http.ResponseWriter, r *http.Request) {
 
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Added email."))
-}
-
-func (h *Handler) SendToAll(w http.ResponseWriter, r *http.Request) {
-	ctx, cancel := context.WithTimeout(r.Context(), time.Second*5)
-	defer cancel()
-
-	if err := h.svc.SendToAll(ctx); err != nil {
-		h.log.Error("Failed to send mailing list", "err", err)
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("Failed to send mailing list."))
-		return
-	}
-
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Mailing lists were successfuly sent!"))
 }
