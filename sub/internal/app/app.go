6ca9147eb5e40c8a2424f8a5ed59ac979bbeed50
Vadym Hrashchenko <vhrashch@cisco.com>
2024-05-18 22:50:10 +0300

    fix: go.mod deps after pkg renaming

--- sub/internal/app/app.go
+++ sub/internal/app/app.go
@@ -23,8 +23,8 @@ import (
 const operation = "app init"
 
 const (
-	cronJobHour   = 12
-	cronJobMinute = 0
+	cronJobHour   = 19
+	cronJobMinute = 55
 )
 
 func New(cfg cfg.Config, log *slog.Logger) *App {
