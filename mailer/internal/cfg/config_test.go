2c6c71e7f2702a3a8e51424e2c12db06164905ee
Vadym Hrashchenko <vhrashch@cisco.com>
2024-05-18 19:26:36 +0300

    feat: mailer app & config tests

---
+++ mailer/internal/cfg/config_test.go
@@ -0,0 +1,130 @@
+package cfg
+
+import (
+	"errors"
+	"os"
+	"reflect"
+	"testing"
+)
+
+func TestMust(t *testing.T) {
+	t.Parallel()
+	type args struct {
+		cfg *Config
+		err error
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *Config
+		wantErr bool
+	}{
+		{
+			name: "Should not panic when err is nil",
+			args: args{
+				cfg: &Config{},
+				err: nil,
+			},
+			want: &Config{},
+		},
+		{
+			name: "Should panic when err is not nil",
+			args: args{
+				cfg: nil,
+				err: errors.New("failed to parse config"),
+			},
+			want:    nil,
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			if tt.wantErr {
+				defer func() {
+					if recover() == nil {
+						t.Fatal("Expected to panic")
+					}
+				}()
+			}
+			if got := Must(tt.args.cfg, tt.args.err); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Must() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestNewFromEnv(t *testing.T) {
+	tests := []struct {
+		name    string
+		setup   func()
+		want    *Config
+		wantErr bool
+	}{
+		{
+			name: "Should parse config correctly when all env vars are present",
+			setup: func() {
+				os.Setenv(logLevelEnvKey, "debug")
+				os.Setenv(portEnvKey, "80")
+				os.Setenv(mailerTokenEnvKey, "secret")
+			},
+			want: &Config{
+				LogLevel:    "debug",
+				Port:        "80",
+				MailerToken: "secret",
+			},
+			wantErr: false,
+		},
+		{
+			name: "Should not parse config correctly when log level is missing",
+			setup: func() {
+				os.Setenv(logLevelEnvKey, "")
+				os.Setenv(portEnvKey, "80")
+				os.Setenv(mailerTokenEnvKey, "secret")
+			},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "Should not parse config correctly when port is missing",
+			setup: func() {
+				os.Setenv(logLevelEnvKey, "debug")
+				os.Setenv(portEnvKey, "")
+				os.Setenv(mailerTokenEnvKey, "secret")
+			},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "Should not parse config correctly when token is missing",
+			setup: func() {
+				os.Setenv(logLevelEnvKey, "debug")
+				os.Setenv(portEnvKey, "80")
+				os.Setenv(mailerTokenEnvKey, "")
+			},
+			want:    nil,
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Cleanup(func() {
+				os.Unsetenv(logLevelEnvKey)
+				os.Unsetenv(portEnvKey)
+				os.Unsetenv(mailerTokenEnvKey)
+			})
+
+			tt.setup()
+			got, err := NewFromEnv()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("NewFromEnv() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewFromEnv() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
