148066ee43faf2d0935caf8eee8e6d6cfcb16c34
Vadym Hrashchenko <vhrashch@cisco.com>
2024-05-18 11:08:04 +0300

    feat: error handling for alredy exists err case

--- sub/internal/storage/subscriber/model.go
+++ sub/internal/storage/subscriber/model.go
@@ -1,42 +1,9 @@
 package subscriber
 
-import (
-	"context"
-	"time"
-
-	"github.com/jmoiron/sqlx"
-)
+import "time"
 
 type Subscriber struct {
 	ID        int64     `db:"id"`
 	Email     string    `db:"email"`
 	CreatedAt time.Time `db:"created_at"`
-}
-
-type Repo struct {
-	db *sqlx.DB
-}
-
-func NewRepo(db *sqlx.DB) *Repo {
-	return &Repo{
-		db: db,
-	}
-}
-
-func (r *Repo) Save(ctx context.Context, s Subscriber) (int64, error) {
-	res, err := r.db.ExecContext(ctx, "INSERT INTO subscribers (email) VALUES (?)", s.Email)
-	if err != nil {
-		return 0, err
-	}
-
-	return res.LastInsertId()
-}
-
-func (r *Repo) Get(ctx context.Context) ([]Subscriber, error) {
-	subscribers := []Subscriber{}
-	if err := r.db.SelectContext(ctx, &subscribers, "SELECT * FROM subscribers"); err != nil {
-		return nil, err
-	}
-
-	return subscribers, nil
 }
