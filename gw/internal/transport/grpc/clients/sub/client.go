05199d3834a2643e7d63e72b4a6ffe944e1aeba0
Vadym Hrashchenko <vhrashch@cisco.com>
2024-05-18 10:31:13 +0300

    feat: added retry middlewares

--- gw/internal/transport/grpc/clients/sub/client.go
+++ gw/internal/transport/grpc/clients/sub/client.go
@@ -4,20 +4,35 @@ import (
 	"context"
 	"fmt"
 	"log/slog"
+	"time"
 
+	"github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/retry"
 	pb "github.com/hrvadl/btcratenotifier/protos/gen/go/v1/sub"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials/insecure"
 
 	"github.com/hrvadl/btcratenotifier/gw/pkg/logger"
 )
 
+const (
+	retryCount   = 3
+	retryTimeout = time.Second * 2
+)
+
 func NewClient(addr string, log *slog.Logger) (*Client, error) {
+	retryOpt := []retry.CallOption{
+		retry.WithCodes(codes.Aborted, codes.NotFound, codes.DeadlineExceeded),
+		retry.WithMax(retryCount),
+		retry.WithPerRetryTimeout(retryTimeout),
+	}
+
 	cc, err := grpc.NewClient(
 		addr,
 		grpc.WithTransportCredentials(insecure.NewCredentials()),
 		grpc.WithChainUnaryInterceptor(
 			logger.NewClientGRPCMiddleware(log),
+			retry.UnaryClientInterceptor(retryOpt...),
 		),
 	)
 	if err != nil {
--- sub/go.mod
+++ sub/go.mod
@@ -7,14 +7,13 @@ require (
 	github.com/grpc-ecosystem/go-grpc-middleware/v2 v2.1.0
 	github.com/hrvadl/btcratenotifier/protos v0.0.0-20240517134945-af8b74789c30
 	github.com/jmoiron/sqlx v1.4.0
+	golang.org/x/net v0.22.0
 	google.golang.org/grpc v1.64.0
 	google.golang.org/protobuf v1.34.1
 )
 
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
-	golang.org/x/net v0.22.0 // indirect
-	golang.org/x/sync v0.7.0 // indirect
 	golang.org/x/sys v0.18.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240318140521-94a12d6c2237 // indirect
