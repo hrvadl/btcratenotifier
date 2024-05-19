b7ce81a641fc582c9b4c42ccc8bce963622e6976
Vadym Hrashchenko <vhrashch@cisco.com>
2024-05-19 18:19:04 +0300

    feat: added godoc comments to the mailer package

--- mailer/internal/cfg/config.go
+++ mailer/internal/cfg/config.go
@@ -13,12 +13,18 @@ const (
 	mailerTokenEnvKey = "MAILER_API_KEY"
 )
 
+// Config struct represents application config,
+// which is used application-wide.
 type Config struct {
 	MailerToken string
 	LogLevel    string
 	Port        string
 }
 
+// Must is a handly wrapper around return results from
+// the NewFromEnv() function, which will panic in case of error.
+// Should be called only in main function, when we don't need
+// to handle errors.
 func Must(cfg *Config, err error) *Config {
 	if err != nil {
 		panic(err)
@@ -26,6 +32,9 @@ func Must(cfg *Config, err error) *Config {
 	return cfg
 }
 
+// NewFromEnv parses the environment variables into
+// the Config struct. Returns an error if any of required variables
+// is missing or contains invalid value.
 func NewFromEnv() (*Config, error) {
 	logLevel := os.Getenv(logLevelEnvKey)
 	if logLevel == "" {
