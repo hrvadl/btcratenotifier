57e28d11e36386ea2bc05e89f955e3de4e424e22
Vadym Hrashchenko <vhrashch@cisco.com>
2024-05-19 19:47:06 +0300

    feat: added godoc comments to the rw

--- main.go
+++ main.go
@@ -2,7 +2,3 @@
 // in a multi-module Go workspace
 // @link https://github.com/golangci/golangci-lint/issues/2654
 package main
-
-// TODO:
-//
-// 1. Add GoDoc comments
--- rw/internal/transport/grpc/server/ratewatcher/handler.go
+++ rw/internal/transport/grpc/server/ratewatcher/handler.go
@@ -12,6 +12,9 @@ import (
 
 const operation = "converted server"
 
+// Registers rate watcher handler to the given GRPC server.
+// NOTE: all parameters are required, the service will panic if
+// either of them is missing.
 func Register(srv *grpc.Server, cnv Converter, log *slog.Logger) {
 	pb.RegisterRateWatcherServiceServer(srv, &Server{
 		log:       log,
@@ -24,12 +27,17 @@ type Converter interface {
 	Convert(ctx context.Context) (float32, error)
 }
 
+// Server represents rate watcher GRPC server
+// which will handle the incoming requests and delegate
+// all work to the underlying converter.
 type Server struct {
 	pb.UnimplementedRateWatcherServiceServer
 	log       *slog.Logger
 	converter Converter
 }
 
+// GetRate method calls underlying converter method and returns an error, in case there was a
+// failure.
 func (s *Server) GetRate(ctx context.Context, _ *emptypb.Empty) (*pb.RateResponse, error) {
 	rate, err := s.converter.Convert(ctx)
 	if err != nil {
