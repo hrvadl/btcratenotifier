5bba1ad4921aac760ed3ebc117f29c51029721e0
Vadym Hrashchenko <vhrashch@cisco.com>
2024-05-19 13:40:56 +0300

    fix: typos

--- main.go
+++ main.go
@@ -5,18 +5,6 @@ package main
 
 // TODO:
 //
-// 1.  Add tests !! (Done)
-// 2.  Add pre-commit (Done)
-// 3.  Add GoDoc comments
-// 4.  Add app schema
-// 5.  Add README to each microservice
-// 6.  Extend root level taskfile (Done)
-// 7.  Add sql already exist check on subscribe (Done)
-// 8.  Add HTTP middlewares (Done)
-// 9.  Docker compose depends on multiple services condition (Done)
-// 10. Add retries to GRPC (Done)
-// 11. Add retries to crypto API (Done)
-// 12. Add CI (GH Actions) (Done)
-// 13. Add mail validator (Need to test)
-// 14. Rename repository !! (Done)
-// 15. Rename rw pkg (Done)
+// 1.  Add GoDoc comments
+// 2.  Add README to each microservice
+// 3. Add mail validator (Need to test)
