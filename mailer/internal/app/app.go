1b1aefaf6b7ba455ff98bf46315f823fbc3a0496
Vadym Hrashchenko <vhrashch@cisco.com>
2024-06-11 22:41:19 +0300

    feat: migrated from pre-commit to left hook

--- mailer/internal/app/app.go
+++ mailer/internal/app/app.go
@@ -10,10 +10,10 @@ import (
 
 	"google.golang.org/grpc"
 
+	"github.com/GenesisEducationKyiv/software-engineering-school-4-0-hrvadl/kit/logger"
 	"github.com/GenesisEducationKyiv/software-engineering-school-4-0-hrvadl/mailer/internal/cfg"
 	"github.com/GenesisEducationKyiv/software-engineering-school-4-0-hrvadl/mailer/internal/platform/mail/resend"
 	"github.com/GenesisEducationKyiv/software-engineering-school-4-0-hrvadl/mailer/internal/transport/grpc/server/mailer"
-	"github.com/GenesisEducationKyiv/software-engineering-school-4-0-hrvadl/kit/logger"
 )
 
 const operation = "app init"
--- rw/cmd/server/main.go
+++ rw/cmd/server/main.go
@@ -4,9 +4,9 @@ import (
 	"log/slog"
 	"os"
 
+	"github.com/GenesisEducationKyiv/software-engineering-school-4-0-hrvadl/kit/logger"
 	"github.com/GenesisEducationKyiv/software-engineering-school-4-0-hrvadl/rw/internal/app"
 	"github.com/GenesisEducationKyiv/software-engineering-school-4-0-hrvadl/rw/internal/cfg"
-	"github.com/GenesisEducationKyiv/software-engineering-school-4-0-hrvadl/kit/logger"
 )
 
 const source = "rateWatcher"
