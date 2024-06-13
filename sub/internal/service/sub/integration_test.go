e9c1c02e452024ed1e0e76a3473302756f1c693e
Vadym Hrashchenko <vhrashch@cisco.com>
2024-06-13 23:13:05 +0300

    chore: rewrote integration test using reusable pkg

--- sub/internal/service/sub/integration_test.go
+++ sub/internal/service/sub/integration_test.go
@@ -5,93 +5,57 @@ package sub
 import (
 	"context"
 	"fmt"
+	"log/slog"
 	"path/filepath"
 	"testing"
 	"time"
 
-	"github.com/golang-migrate/migrate/v4"
-	_ "github.com/golang-migrate/migrate/v4/database/mysql"
-	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"github.com/GenesisEducationKyiv/software-engineering-school-4-0-hrvadl/pkg/tests"
 	"github.com/stretchr/testify/require"
-	"github.com/testcontainers/testcontainers-go"
-	mysqlc "github.com/testcontainers/testcontainers-go/modules/mysql"
+	"github.com/testcontainers/testcontainers-go/modules/mysql"
 
 	"github.com/GenesisEducationKyiv/software-engineering-school-4-0-hrvadl/sub/internal/service/validator"
 	"github.com/GenesisEducationKyiv/software-engineering-school-4-0-hrvadl/sub/internal/storage/platform/db"
 	"github.com/GenesisEducationKyiv/software-engineering-school-4-0-hrvadl/sub/internal/storage/subscriber"
 )
 
-var mySQLContainer *mysqlc.MySQLContainer
-
-const (
-	dbName   = "test_converter"
-	host     = "localhost"
-	port     = 3306
-	user     = "root"
-	password = "password"
-)
-
-func mustGetDSN(c *mysqlc.MySQLContainer) string {
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
-	defer cancel()
-	dsn, err := c.ConnectionString(ctx, "parseTime=true", "tls=skip-verify")
-	if err != nil {
-		panic(err)
-	}
-	return dsn
-}
-
-func toFileScheme(path string) string {
-	return "file://" + path
-}
-
-func toMySQLScheme(dsn string) string {
-	return "mysql://" + dsn
-}
-
-func setup(ctx context.Context) error {
-	var err error
-	mySQLContainer, err = mysqlc.RunContainer(ctx,
-		testcontainers.WithImage("mysql:latest"),
-		mysqlc.WithDatabase(dbName),
-		mysqlc.WithUsername(user),
-		mysqlc.WithPassword(password),
-	)
-	if err != nil {
-		return fmt.Errorf("failed to create container: %w", err)
-	}
-
-	migrations, err := filepath.Abs(filepath.Join("../../../", "migrations"))
-	if err != nil {
-		return fmt.Errorf("failed to find migrations dir %s: %w", migrations, err)
-	}
-
-	m, err := migrate.New(toFileScheme(migrations), toMySQLScheme(mustGetDSN(mySQLContainer)))
-	if err != nil {
-		return fmt.Errorf("failed to run migrations: %w", err)
-	}
-
-	return m.Up()
-}
-
-func teardown(ctx context.Context) error {
-	return mySQLContainer.Terminate(ctx)
-}
+var mySQLContainer *mysql.MySQLContainer
 
 func TestMain(m *testing.M) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Minute*1)
 	defer cancel()
-	if err := setup(ctx); err != nil {
+
+	var err error
+	mySQLContainer, err = tests.NewDB(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		tctx, tcancel := context.WithTimeout(context.Background(), time.Second*10)
+		defer tcancel()
+		if err = mySQLContainer.Terminate(tctx); err != nil {
+			slog.Info("Failed to terminate test container", slog.Any("err", err))
+		}
+	}()
+
+	migrations, err := filepath.Abs(filepath.Join("../../../", "migrations"))
+	if err != nil {
+		panic(fmt.Sprintf("failed to find migrations dir %s: %v", migrations, err))
+	}
+
+	ctx, cancel = context.WithTimeout(context.Background(), time.Minute*1)
+	defer cancel()
+	conn, err := mySQLContainer.ConnectionString(ctx)
+	if err != nil {
+		panic(fmt.Sprintf("failed to get conn string from db container: %v", err))
+	}
+
+	if err := tests.MigrateDB(migrations, conn); err != nil {
 		panic(err)
 	}
 
 	exitCode := m.Run()
-
-	ctx, cancel = context.WithTimeout(context.Background(), time.Minute*1)
-	defer cancel()
-	if err := teardown(ctx); err != nil {
-		panic(err)
-	}
 
 	if exitCode != 0 {
 		panic("test run returned negative exit code")
@@ -103,7 +67,7 @@ func TestServiceSend(t *testing.T) {
 		ctx  context.Context
 		mail string
 	}
-	tests := []struct {
+	testCases := []struct {
 		name    string
 		args    args
 		wantErr bool
@@ -134,9 +98,9 @@ func TestServiceSend(t *testing.T) {
 		},
 	}
 
-	for _, tt := range tests {
+	for _, tt := range testCases {
 		t.Run(tt.name, func(t *testing.T) {
-			db, err := db.NewConn(mustGetDSN(mySQLContainer))
+			db, err := db.NewConn(tests.MustGetDSN(mySQLContainer))
 			require.NoError(t, err, "Failed to connect to db")
 
 			rs := subscriber.NewRepo(db)
