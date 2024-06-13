31fa8b4380c750c96e2a0d097202974f9b8c6efc
Vadym Hrashchenko <vhrashch@cisco.com>
2024-06-13 19:30:36 +0300

    fix: removed rename script

--- rename.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-export CUR="github.com/GenesisEducationKyiv/software-engineering-school-4-0-hrvadl/kit/logger"
-export NEW="github.com/GenesisEducationKyiv/software-engineering-school-4-0-hrvadl/pkg/logger"
-go mod edit -module ${NEW}
-find . -type f -name '*.go' -exec perl -pi -e 's/$ENV{CUR}/$ENV{NEW}/g' {} \;
-
