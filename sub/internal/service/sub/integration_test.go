e0396545b77872b728068e018dbd9559d731192d
Vadym Hrashchenko <vhrashch@cisco.com>
2024-06-14 11:09:49 +0300

    feat: correct tests to use db setup in taskfile

--- sub/internal/service/sub/integration_test.go
+++ sub/internal/service/sub/integration_test.go
@@ -4,62 +4,19 @@ package sub
 
 import (
 	"context"
-	"fmt"
-	"log/slog"
-	"path/filepath"
+	"os"
 	"testing"
 	"time"
 
-	"github.com/GenesisEducationKyiv/software-engineering-school-4-0-hrvadl/pkg/tests"
 	"github.com/stretchr/testify/require"
-	"github.com/testcontainers/testcontainers-go/modules/mysql"
 
 	"github.com/GenesisEducationKyiv/software-engineering-school-4-0-hrvadl/sub/internal/service/validator"
 	"github.com/GenesisEducationKyiv/software-engineering-school-4-0-hrvadl/sub/internal/storage/platform/db"
 	"github.com/GenesisEducationKyiv/software-engineering-school-4-0-hrvadl/sub/internal/storage/subscriber"
 )
 
-var mySQLContainer *mysql.MySQLContainer
-
 func TestMain(m *testing.M) {
-	ctx, cancel := context.WithTimeout(context.Background(), time.Minute*1)
-	defer cancel()
-
-	var err error
-	mySQLContainer, err = tests.NewDB(ctx)
-	if err != nil {
-		panic(err)
-	}
-
-	defer func() {
-		tctx, tcancel := context.WithTimeout(context.Background(), time.Second*10)
-		defer tcancel()
-		if err = mySQLContainer.Terminate(tctx); err != nil {
-			slog.Info("Failed to terminate test container", slog.Any("err", err))
-		}
-	}()
-
-	migrations, err := filepath.Abs(filepath.Join("../../../", "migrations"))
-	if err != nil {
-		panic(fmt.Sprintf("failed to find migrations dir %s: %v", migrations, err))
-	}
-
-	ctx, cancel = context.WithTimeout(context.Background(), time.Minute*1)
-	defer cancel()
-	conn, err := mySQLContainer.ConnectionString(ctx)
-	if err != nil {
-		panic(fmt.Sprintf("failed to get conn string from db container: %v", err))
-	}
-
-	if err := tests.MigrateDB(migrations, conn); err != nil {
-		panic(err)
-	}
-
-	exitCode := m.Run()
-
-	if exitCode != 0 {
-		panic("test run returned negative exit code")
-	}
+	os.Exit(m.Run())
 }
 
 func TestServiceSend(t *testing.T) {
@@ -116,7 +73,9 @@ func TestServiceSend(t *testing.T) {
 
 	for _, tt := range testCases {
 		t.Run(tt.name, func(t *testing.T) {
-			db, err := db.NewConn(tests.MustGetDSN(mySQLContainer))
+			dsn := os.Getenv("SUB_DSN")
+			require.NotZero(t, dsn, "test DSN can not be empty")
+			db, err := db.NewConn(dsn)
 			require.NoError(t, err, "Failed to connect to db")
 
 			rs := subscriber.NewRepo(db)
