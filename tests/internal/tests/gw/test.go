9ebff8353eeb0945c62e19d0da484b5582519648
Vadym Hrashchenko <vhrashch@cisco.com>
2024-06-15 13:10:07 +0300

    chore: moved all infra related files to separate dir

--- tests/internal/tests/gw/test.go
+++ tests/internal/tests/gw/test.go
@@ -9,11 +9,13 @@ import (
 	"github.com/tsenart/vegeta/v12/lib/prom"
 )
 
-const frequency = 100
+const (
+	frequency = 1000
+	duration  = 5 * time.Minute
+)
 
 func AttackGetRate(pm *prom.Metrics, url string) {
 	rate := vegeta.Rate{Freq: frequency, Per: time.Second}
-	duration := 1 * time.Minute
 	targeter := vegeta.NewStaticTargeter(vegeta.Target{
 		Method: http.MethodGet,
 		URL:    fmt.Sprintf("http://%s/api/rate", url),
