8f29616a561389375d05e54e8fdec2f9f6c9c588
hrvadl <hrvadleo@gmail.com>
2024-06-23 12:40:59 +0300

    chore: renamed formatter

--- sub/internal/service/sender/formatter/html.go
+++ sub/internal/service/sender/formatter/html.go
@@ -6,19 +6,19 @@ import (
 )
 
 // NewWithDate constructs new HTML formatter for mails.
-func NewWithDate() *WithDateFormatter {
-	return &WithDateFormatter{}
+func NewWithDate() *WithDate {
+	return &WithDate{}
 }
 
-// WithDateFormatter is a HTML formatter for mails,
+// WithDate is a HTML formatter for mails,
 // which will include date in the message and format float to
 // 2 point precision.
-type WithDateFormatter struct{}
+type WithDate struct{}
 
 // Format method taking exchange rate as a argument, then
 // includes date in the message and formats float to
 // 2 point precision.
-func (hf *WithDateFormatter) Format(r float32) string {
+func (hf *WithDate) Format(r float32) string {
 	return fmt.Sprintf(
 		"Latest exchange rate as for %v: 1 USD worth %.2f UAH",
 		time.Now().Format(time.DateTime),
